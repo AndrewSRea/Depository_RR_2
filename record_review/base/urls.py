eb143e8eb9009f3064ae15ad4a4ab84d7e8d1f42
Andrew Rea <andrew.s.rea@gmail.com>
2022-12-30 16:20:50 -0800

    Adding the 'urls.py' file

---
+++ record_review/base/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+  path('login/', views.loginPage, name="login"),
+  path('logout/', views.logoutUser, name="logout"),
+  path('register/', views.registerPage, name="register"),
+
+  path('', views.home, name="home"),
+  path('review/<str:pk>/', views.review, name="review"),
+
+  path('create-review/', views.createReview, name="create-review"),
+  path('update-review/<str:pk>/', views.updateReview, name="update-review"),
+  path('delete-review/<str:pk>/', views.deleteReview, name="delete-review"),
+  path('delete-added-review/<str:pk>/', views.deleteAddedReview, name="delete-added-review")
+]
