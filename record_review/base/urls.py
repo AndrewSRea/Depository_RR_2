1a98d14223b253db580a0893534515d56f32d069
Andrew Rea <andrew.s.rea@gmail.com>
2023-04-13 18:38:50 -0700

    Saving altered file for my 'Record_Review' Django app

--- record_review/base/urls.py
+++ record_review/base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-from . import views
+from . import views 
+
 
 urlpatterns = [
   path('login/', views.loginPage, name="login"),
@@ -8,10 +9,12 @@ urlpatterns = [
 
   path('', views.home, name="home"),
   path('review/<str:pk>/', views.review, name="review"),
-  path('profile/<str:pk>', views.userProfile, name="user-profile"),
+  path('profile/<str:pk>/', views.userProfile, name="user-profile"),
 
   path('create-review/', views.createReview, name="create-review"),
   path('update-review/<str:pk>/', views.updateReview, name="update-review"),
   path('delete-review/<str:pk>/', views.deleteReview, name="delete-review"),
-  path('delete-added-review/<str:pk>/', views.deleteAddedReview, name="delete-added-review")
+  path('delete-added-review/<str:pk>/', views.deleteAddedReview, name="delete-added-review"),
+
+  path('update-profile/', views.updateProfile, name="update-profile"),
 ]
