28b31df82739b711e2c09c6b9e32c7138fefd083
Andrew Rea <andrew.s.rea@gmail.com>
2023-04-13 18:37:12 -0700

    Saving altered file for my 'Record_Review' Django app

--- record_review/base/models.py
+++ record_review/base/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Review(models.Model):
   artist = models.CharField(max_length=100)
   album = models.CharField(max_length=200)
-  rating = models.IntegerField(default=10)
+  rating = models.IntegerField(
+    validators=[MinValueValidator(0), MaxValueValidator(10)],
+    default=10,
+  )
+  # change the name of `content` with `verbose` for the future form?
   content = models.TextField(null=True, blank=True)
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(default=timezone.now) # date_posted
@@ -20,17 +25,11 @@ class Review(models.Model):
     return '%s - %s' % (self.artist, self.album)
 
 
-# REMEMBER! THIS IS A BREAKABLE PROJECT! Nothing here is written in stone.
-
-# Just learned that any model relating to another model needs to be placed above the model to which it relates.
-# i.e. in the instructor's example, he has a `Topic` model, and `topic` is a field in his `Room` model.
-# Therefore, he has his `Topic` model above his `Room` model.
-
-
 class AddedReview(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  review = models.ForeignKey(Review, on_delete=models.CASCADE) # CASCADE will delete all additional reviews in an initial review when the initial review is deleted
+  review = models.ForeignKey(Review, on_delete=models.CASCADE)
   rating = models.IntegerField(default=10)
+  # again, change the name of `body` with `verbose` for the future form?
   body = models.TextField()
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(default=timezone.now) # date_posted
@@ -39,4 +38,4 @@ class AddedReview(models.Model):
     ordering = ['-updated', '-created']
 
   def __str__(self):
-    return self.body[0:50] # This part can show in the right column when a user is logged in to show them recent reviews on their own reviews
+    return self.body[0:50]
