ec54b66a8e29a44ee9cf9c6cbff2eee8a35b45a5
Andrew Rea <andrew.s.rea@gmail.com>
2023-05-07 16:37:40 -0700

    Saving altered file for my 'Record_Review' Django app

--- record_review/base/models.py
+++ record_review/base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Count
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
@@ -17,6 +18,30 @@ class Review(models.Model):
   created = models.DateTimeField(default=timezone.now) # date_posted
   author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
   # image =
+
+  def avg_rating(self):
+    added_ratings = 0
+    orig_rating = self.rating
+    added_ratings_count = self.addedreview_set.count()
+
+    if added_ratings_count > 0:
+      for added_review in self.addedreview_set.all():
+        added_ratings += added_review.rating
+        ratings_sum = added_ratings + orig_rating
+
+      ratings_count = added_ratings_count + 1
+      ratings_avg = ratings_sum / ratings_count
+      avg_return = round(ratings_avg, 1)
+
+      if avg_return == 10.0:
+        avg_return = 10
+      else:
+        avg_return = avg_return
+
+      return avg_return
+
+    else:
+      return self.rating
 
   class Meta:
     ordering = ['-updated', '-created']
@@ -38,4 +63,4 @@ class AddedReview(models.Model):
     ordering = ['-updated', '-created']
 
   def __str__(self):
-    return self.body[0:50]
+    return '%s - %s' % (self.user, self.body[0:50])
