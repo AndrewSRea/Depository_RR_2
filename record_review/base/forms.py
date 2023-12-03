7d3475db1c8887ca6a5823a525864a2dd3550c77
Andrew Rea <andrew.s.rea@gmail.com>
2023-12-03 09:09:21 -0800

    Updating file

--- record_review/base/forms.py
+++ record_review/base/forms.py
@@ -1,42 +1,35 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import gettext_lazy as _
-from .models import Review
-from django.contrib.auth.models import User
+from .models import Album, User
 
 
-class ReviewForm(ModelForm):
+class MyUserCreationForm(UserCreationForm):
   class Meta:
-    model = Review 
+    model = User
+    fields = ['username', 'password1', 'password2']
+
+
+class AlbumForm(ModelForm):
+  class Meta:
+    model = Album
     fields = '__all__'
-    exclude = ['created', 'author']
+    exclude = ['creator', 'created']
     labels = {
-      'rating': _('Rating (0-10)'),
-      'content': _('Your review'),
+      'artist': _('Artist or Band Name'),
+      'title': _('Album Title'),
+      'image': _('Album Image'),
+      'rating': _('Rating (1-10)'),
+      'comment': _('Your Review'),
     }
 
-
-# RE: the `image` field: "...files that are uploaded using a form need to be handled
-# differently (they can be retrieved from `request.FILES`, rather than `request.POST).
-# For details of how to handle file uploads with your form, see 'Binding uploaded files
-# to a form': https://docs.djangoproject.com/en/4.1/ref/forms/api/#binding-uploaded-files-to-a-form"
-
-# ^^ When you want to include the 'rating', delete `exclude = ...` and replace it with
-# `fields = '__all__'`, save the code, then replace `fields = ...` with `exclude = ...`
-# again.
-
-
-# Hmmm...replace "Content" with "Review"? ("Content" is not very descriptive for a User
-# when looking at the Review Form.) (Can I put "review" as field inside the "Review" model?)
-# ^^^ I believe this can be changed with `verbose_name`.
-
-# See the Django documentation: 
-# https://docs.djangoproject.com/en/4.1/topics/forms/modelforms/#selecting-the-fields-to-use
-
-# Buy into Dennis Ivy's full Django course to learn more about form customization?
-# https://dennisivy.teachable.com/p/django-beginners-course/?product_id=3222835&coupon_code=BRAD
-
+# when you add an image to the form, delete `exclude = []`, then add `'creator', 'image', 'created'` to `exclude`
+# See your notes in Github
 
 class UserForm(ModelForm):
   class Meta:
     model = User
-    fields = ['username']
+    fields = ['image', 'username', 'bio']
+    labels = {
+      'image': _('Profile Photo'),
+    }
